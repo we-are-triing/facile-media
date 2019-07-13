1d691b396aa94afd252457a7199ee35b7ed9ce78
Colt Pini <coltpini@luce.studio>
2019-07-13 13:25:06 -0700

    starting media, upload is working. This server stores the raw images, and the db server stores all the data. the name of the file is the key.

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,57 @@
 import joi from '@hapi/joi';
+import {promises} from 'fs';
+import {join} from 'path';
+import fetch from 'node-fetch';
+import boom from '@hapi/boom';
+import random from './utils/random.js';
+
+const nameValidation = joi
+  .string()
+  .regex(/[a-zA-Z0-9 _.-]/)
+  .required();
+
+const mediaValidation = joi.object({
+  tags: joi.array().items(joi.string()),
+  name: nameValidation,
+  meta: joi.object({
+    name: joi.string(),
+    lastModified: joi.number(),
+    size: joi.number(),
+    type: joi.string()
+  }),
+  media: joi.binary()
+});
+
+const filenameValidation = {
+  filename: nameValidation
+};
+
+const dataDomain = `http://api:8001`;
+
+const getExt = type => {
+  switch (type) {
+    case `image/jpeg`:
+      return `jpg`;
+      break;
+    case `image/png`:
+      return `png`;
+      break;
+    case `image/gif`:
+      return `gif`;
+      break;
+    case `image/webp`:
+      return `webp`;
+      break;
+    case `image/svg+xml`:
+      return `xml`;
+      break;
+    case `video/mp4`:
+      return `mp4`;
+      break;
+    default:
+      return `unknown`;
+  }
+};
 
 export default server => {
   server.route([
@@ -7,54 +60,71 @@ export default server => {
       path: `/{param*}`,
       handler: {
         directory: {
-          path: 'client'
+          path: './media'
+        }
+      }
+    },
+    {
+      method: `GET`,
+      path: `/api/{filename}`,
+      options: {
+        description: `get modified images`,
+        notes: `get images that can be modified.`,
+        tags: [`api`, `media`],
+        validate: {
+          params: filenameValidation
+        }
+      },
+      handler: async (req, h) => {
+        try {
+          //const raw = await fetch(`${dataDomain}/content/component/${type}`);
+          //const [component] = await raw.json();
+        } catch (err) {
+          console.error(``, err);
+          return boom.badImplementation(`something done broke`);
         }
       }
     },
     {
       method: `POST`,
-      path: `/{filename}`,
+      path: `/media`,
       options: {
         description: `Updates templates`,
         notes: `Update a template ready to be used.`,
-        tags: [`api`, `content`, `template`],
+        tags: [`api`, `media`],
+        payload: {
+          maxBytes: 1000 * 1000 * 10
+        },
         validate: {
-          payload: {
-            meta: joi.object({
-              type: joi.string().required(),
-              icon: joi.string().required(),
-              tags: joi.array().items(joi.string())
-            }),
-            values: joi.array().items(
-              joi
-                .object({
-                  name: joi
-                    .string()
-                    .regex(/[a-zA-Z _-]/)
-                    .required(),
-                  type: joi
-                    .string()
-                    .valid(`string`, `text`, `text_block`, `number`, `boolean`, `object`, `region`, `set`, `list`)
-                    .required()
-                })
-                .when(joi.object({type: `region`}).unknown(), {
-                  then: joi.object({
-                    region: joi
-                      .string()
-                      .valid(`fluid`, `fixed`, `single`, `static`)
-                      .required(),
-                    components: joi.array().items(joi.string())
-                  })
-                })
-            )
-          }
+          payload: mediaValidation
         }
       },
       handler: async (req, h) => {
         try {
+          // TODO: random generation
+          const {tags, name, media, meta} = req.payload;
+          const filename = `${random(15)}.${getExt(meta.type)}`;
+          const master = 'self';
+          const media64 = media.toString('utf-8');
+          const sm = media64.split(',')[1];
+          const buf = Buffer.from(sm, 'base64');
+
+          await promises.writeFile(join(`./media`, `${filename}`), buf);
+          const body = JSON.stringify({tags, name, filename, meta, master});
+
+          const raw = await fetch(`${dataDomain}/media`, {
+            headers: {'Content-Type': 'application/json'},
+            method: 'POST',
+            body
+          });
+
+          // TODO: do a better media base URL system.
+          return {
+            path: `http://localhost:8002/${filename}`
+          };
         } catch (err) {
           console.error(``, err);
-          return `fourOFour()`;
+          return boom.badImplementation(`something done broke`);
         }
       }
     }
